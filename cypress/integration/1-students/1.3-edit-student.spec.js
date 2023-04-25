f5d37de4f491b967a9c4e2da9674e74809602948
LourenaOhara <lourena6ohara@gmail.com>
2023-04-25 20:10:01 -0300

    Add Doc and Form Student Test

--- cypress/integration/1-students/1.3-edit-student.spec.js
+++ cypress/integration/1-students/1.3-edit-student.spec.js
@@ -2,16 +2,30 @@
 import DetailLogin from "../../support/pages/detailLogin";
 import DetailStudents from "../../support/pages/detailStudents";
 
+const uuid = () => Cypress._.random(0, 1e6)
+const numb = uuid()
 
 describe('Students', () => {
-    it("Search Student", () => {
+    it("Edit Student", () => {
         DetailLogin.Login()
         DetailStudents.clickStudentsLink()
         DetailStudents.validStudentCard()
         DetailStudents.validStudentCardName('João Fernando Seixas')
         DetailStudents.clickEditStudent()
-        DetailStudents.typeEditCellNumber()
+        DetailStudents.typeEditCellNumber('3192999111'+numb)
         DetailStudents.clickBtnEditStudent()
         DetailStudents.validModalSuccess()
     })
+    //thos scenario should return a error. But the test passed
+    it("Edit Student - Invalid Data", () => {
+        DetailLogin.Login()
+        DetailStudents.clickStudentsLink()
+        DetailStudents.validStudentCard()
+        DetailStudents.validStudentCardName('João Fernando Seixas')
+        DetailStudents.clickEditStudent()
+        DetailStudents.typeEditCellNumber('319297')
+        DetailStudents.clickBtnEditStudent()
+        DetailStudents.validModalSuccess()
+    })
+
 })
